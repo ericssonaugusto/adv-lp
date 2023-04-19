d74f70bcb94a63838be02dad535982d72be41d7f
Ericsson Augusto <ericssonaugusto@gmail.com>
2023-04-19 08:52:30 -0300

    Accordion Abre e fecha

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -1,37 +1,28 @@
 import 'tailwindcss/tailwind.css'
-import { useState } from 'react'
+import {Collapse} from 'react-collapse'
+import{ AiOutlineMinus,AiOutlinePlus } from 'react-icons/ai'
 
-const Accordion = ({ title, content }: { title: string; content: string }) => {
-    const [isOpen, setIsOpen] = useState(false);
-  
-    return (
+const Accordion = ({ open, toggle, title, content }: { title: string; content: string; open: boolean; toggle: any }) => {
+    
+return (
       <div className='w-4/6 mx-auto'>
-        <button
-          onClick={() => setIsOpen(!isOpen)}
+        <div
+          onClick={toggle}
           className="flex items-center justify-between w-full px-4 py-2 text-lg font-medium text-left text-gray-700 bg-gray-100 rounded-lg hover:bg-gray-200 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50"
         >
-          <span>{title}</span>
-          <svg
-            className={`${isOpen ? 'transform rotate-180' : ''} w-5 h-5`}
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            aria-hidden="true"
-          >
-            <path
-              fillRule="evenodd"
-              d="M6.293 6.293a1 1 0 0 1 1.414 0L10 8.586l2.293-2.293a1 1 0 0 1 1.414 1.414l-3 3a1 1 0 0 1-1.414 0l-3-3a1 1 0 0 1 0-1.414z"
-              clipRule="evenodd"
-            />
-          </svg>
-        </button>
-        {isOpen && (
-          <div className="p-4 mt-4 text-gray-700 bg-gray-100 rounded-lg shadow-md">
+          <h3>{title}</h3>
+          <div className='text-[22px]'>
+            {open ? <AiOutlineMinus/> : <AiOutlinePlus/>}
+          </div>
+        </div>
+        <Collapse isOpened={open}>
+        <div className="p-4 mt-4 text-gray-700 bg-gray-100 rounded-lg shadow-md">
             {content}
           </div>
-        )}
+        </Collapse>
+         
       </div>
-    );
+    )
   }
   export default Accordion;
   
--- src/components/Faq.tsx
+++ src/components/Faq.tsx
@@ -1,14 +1,50 @@
+import { useState } from 'react'
 import Accordion from '../components/Accordion';
 
+
 export function Faq(){
+ 
+  const [open, setOpen] = useState(false);
+  const toggle = (index:any) => {
+    if (open === index) {
+      return setOpen(null)
+    }
+    setOpen(index)
+  }
+
+  const accordionData = [
+    {
+      title: "Quando devo procurar um advogado?",
+      content: "Texto da resposta"
+    },
+    {
+      title: "Advogado Paloma realmente é o melhor?",
+      content: "Texto da resposta"
+    },
+    {
+      title: "Quanto custa uma consulta inicial?",
+      content: "Texto da resposta"
+    },
+    {
+      title: "Vou precisar de um avalista?",
+      content: "Texto da resposta"
+    },
+  ]
+  
     return(
     <div className='grid gap-4 w-full py-20 bg-zinc-500 items-center'>
-        <h2 className='text-3xl text-center text-white font-bold'>Principais duvidas</h2>
-      <Accordion title="Quando devo procurar um advogado?" content="Texto da resposta" />
-      <Accordion title="Advogado Paloma realmente é o melhor?" content="Texto da resposta" />
-      <Accordion title="Quanto custa uma consulta inicial?" content="Texto da resposta" />
-      <Accordion title="Vou precisar de um avalista?" content="Texto da resposta" />
-      <Accordion title="Preciso usar terno no tribunal?" content="Texto da resposta" />
+      <h2 className='text-3xl text-center text-white font-bold'>Principais duvidas</h2>
+      {accordionData.map((data, index) => {
+        return <Accordion 
+        key={index} 
+        open={index === open} 
+        title={data.title} 
+        content={data.content} 
+        toggle={() => toggle(index)}
+        />
+      }
+      )
+      }
     </div>
 
     )
